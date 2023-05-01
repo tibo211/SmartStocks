fb55d5579a82cec133f5b84f8e0db51a7ca10191
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2023-05-01 20:14:59 +0200

    Initial Commit

---
+++ SmartStocks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SmartStocks
+//
+//  Created by Tibor Felföldy on 2023-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartStocks/SmartStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStocksApp.swift
+//  SmartStocks
+//
+//  Created by Tibor Felföldy on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct SmartStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
